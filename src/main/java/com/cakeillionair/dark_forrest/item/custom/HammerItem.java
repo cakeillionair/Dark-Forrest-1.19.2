9384a05806b1c164ae6fe121debd60e079cf5b1a
cakeillionair <117606327+cakeillionair@users.noreply.github.com>
2022-11-10 10:03:55 -0400

    added custom tooltips to the Hammer added items:  -none added blocks:  -none added recipes:  -none added loottables:  -blocks:   -none

--- src/main/java/com/cakeillionair/dark_forrest/item/custom/HammerItem.java
+++ src/main/java/com/cakeillionair/dark_forrest/item/custom/HammerItem.java
@@ -1,6 +1,9 @@
 package com.cakeillionair.dark_forrest.item.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
@@ -13,6 +16,7 @@ import net.minecraft.world.entity.LightningBolt;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.server.level.ServerLevel;
@@ -23,14 +27,26 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
+import java.util.List;
 import java.util.UUID;
 
 public class HammerItem extends Item {
     public HammerItem(Properties properties) {
         super(properties);
     }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag) {
+        if (Screen.hasShiftDown()) {
+            components.add(Component.literal("Right click to summon lightning!").withStyle(ChatFormatting.AQUA));
+        }else {
+            components.add(Component.literal("Press SHIFT for more info").withStyle(ChatFormatting.YELLOW));
+        }
+        super.appendHoverText(itemStack, level, components, tooltipFlag);
+    }
+
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
 
