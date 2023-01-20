e7e64ea47c97458b72260fe4a755720647f54733
Jan <keks.coc@gmx.de>
2023-01-19 23:35:17 -0400

    added DarkwoodLeaves class added items:  -none added blocks:  -Darkwood Leaves  -Darkwood Log added recipes:  -none added loottables:  -blocks:   -darkwood_log

---
+++ src/main/java/com/cakeillionair/dark_forrest/block/custom/DarkGrassBlock.java
@@ -0,0 +1,9 @@
+package com.cakeillionair.dark_forrest.block.custom;
+
+import net.minecraft.world.level.block.CropBlock;
+
+public class DarkGrassBlock extends CropBlock {
+    public DarkGrassBlock(Properties properties) {
+        super(properties);
+    }
+}
--- src/main/java/com/cakeillionair/dark_forrest/textures/missing.java
+++
@@ -1,8 +0,0 @@
-//////////////////
-//// TEXTURES ////
-//////////////////
-missing:
-stripped_darkwood_log,
-stripped_darkwood_log_top,
-darkwood_planks,
-darkwood_sapling
