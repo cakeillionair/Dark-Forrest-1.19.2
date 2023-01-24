e62cd6f9c5de2db81b4cf10bd47969b1634bb1d5
CakesStuff <95542792+CakesStuff@users.noreply.github.com>
2023-01-23 21:31:38 -0400

    add ore generation

--- src/main/java/com/cakeillionair/dark_forrest/world/feature/ModConfiguredFeatures.java
+++ src/main/java/com/cakeillionair/dark_forrest/world/feature/ModConfiguredFeatures.java
@@ -2,31 +2,41 @@ package com.cakeillionair.dark_forrest.world.feature;
 
 import com.cakeillionair.dark_forrest.Dark_Forrest;
 import com.cakeillionair.dark_forrest.block.ModBlocks;
+import com.google.common.base.Suppliers;
 import net.minecraft.core.Registry;
+import net.minecraft.data.worldgen.features.OreFeatures;
 import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.WeightedPlacedFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.RandomFeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.AcaciaFoliagePlacer;
-import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
-import net.minecraft.world.level.levelgen.feature.foliageplacers.FancyFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
-import net.minecraft.world.level.levelgen.feature.trunkplacers.FancyTrunkPlacer;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.ForkingTrunkPlacer;
-import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 public class ModConfiguredFeatures {
     public static final DeferredRegister<ConfiguredFeature<?,?>> CONFIGURED_FEATURES =
             DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, Dark_Forrest.MOD_ID);
 
+    public static final Supplier<List<OreConfiguration.TargetBlockState>> DF_DIM_DARK_ORES =
+            Suppliers.memoize(() -> List.of(
+                OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, ModBlocks.DARK_ORE.get().defaultBlockState())
+            ));
+
+    public static final RegistryObject<ConfiguredFeature<?,?>> DARK_ORE = CONFIGURED_FEATURES.register("dark_ore",()->
+        new ConfiguredFeature<>(Feature.ORE,new OreConfiguration(DF_DIM_DARK_ORES.get(),7)));
+    ///////////////
+    //// TREES ////
+    ///////////////
     public static final RegistryObject<ConfiguredFeature<?,?>> DARKWOOD =
             CONFIGURED_FEATURES.register("darkwood",() ->
                     //TODO : FIX UGLY AS F***
--- src/main/java/com/cakeillionair/dark_forrest/world/feature/ModPlacedFeatures.java
+++ src/main/java/com/cakeillionair/dark_forrest/world/feature/ModPlacedFeatures.java
@@ -5,6 +5,7 @@ import com.cakeillionair.dark_forrest.block.ModBlocks;
 import com.google.common.collect.ImmutableList;
 import net.minecraft.core.Registry;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.placement.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -16,6 +17,25 @@ public class ModPlacedFeatures {
     public static final DeferredRegister<PlacedFeature> PLACED_FEATURES =
             DeferredRegister.create(Registry.PLACED_FEATURE_REGISTRY, Dark_Forrest.MOD_ID);
 
+    public static final RegistryObject<PlacedFeature> DARK_ORE_PLACED = PLACED_FEATURES.register("dark_ore_placed",
+            ()-> new PlacedFeature(ModConfiguredFeatures.DARK_ORE.getHolder().get(),
+                    commonOrePlacement(7,
+                            HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(-80),VerticalAnchor.aboveBottom(80)))));
+
+    private static List<PlacementModifier> orePlacement(PlacementModifier p_195347_, PlacementModifier p_195348_) {
+        return List.of(p_195347_, InSquarePlacement.spread(), p_195348_, BiomeFilter.biome());
+    }
+
+    private static List<PlacementModifier> commonOrePlacement(int p_195344_, PlacementModifier p_195345_) {
+        return orePlacement(CountPlacement.of(p_195344_), p_195345_);
+    }
+
+    private static List<PlacementModifier> rareOrePlacement(int p_195350_, PlacementModifier p_195351_) {
+        return orePlacement(RarityFilter.onAverageOnceEvery(p_195350_), p_195351_);
+    }
+    //////////////
+    //// TREE ////
+    //////////////
     public static final RegistryObject<PlacedFeature> DARKWOOD_CHECKED = PLACED_FEATURES.register("darkwood_checked",
             () -> new PlacedFeature(ModConfiguredFeatures.DARKWOOD.getHolder().get(),
                     List.of(PlacementUtils.filteredByBlockSurvival(ModBlocks.DARKWOOD_SAPLING.get()))));
